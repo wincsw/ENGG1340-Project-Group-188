cbedc3778c52a3465b90a148242adfc112b9b52a
adhk852 <u3574506@connect.hku.hk>
2021-03-10 13:31:43 +0800

    Update draw.cpp

--- draw.cpp
+++ draw.cpp
@@ -1,3 +1,4 @@
+# include <iostream>
 # include <string>
 # include <cstdlib>
 # include <ctime>
@@ -7,37 +8,45 @@
 
 struct item draw() 
 {
-  struct item choosen;
-  srand(time(NULL));
-  int num = rand() % 100 + 1;
+  struct item choosen; // the resturn item
+
+  // random number generater
+  srand(time(NULL)); // set seed to time
+  int num = rand() % 100 + 1; // random in range [1, 100]
+
+  // draw water item
   if (num <= 20){
-    choosen = water(num % 4 + 1, " ");
+    choosen = water(num % 4 + 1);
   }
+  // draw food item
   else if (num <= 40) {
-    choosen = food(num % 4 + 1, " ");
+    choosen = food(num % 4 + 1);
   }
+  // draw medicine item
   else if (num <= 60) {
     int temp = num % 5;
     if (temp <= 2) {
-      choosen = medicine(temp, " ");
+      choosen = medicine(temp);
     }
     else if (temp % 5 <= 1) {
-      choosen = medicine(3, " ");
+      choosen = medicine(3);
     }
     else {
-      choosen = medicine(4, " ");
+      choosen = medicine(4);
     }
   }
+  // draw weapon item
   else if (num <= 90) {
-    choosen = weapon(num % 2 + 1, " ");
+    choosen = weapon(num % 2 + 1);
   }
+  // draw mystery item
   else {
     int temp = num % 10;
     if (temp < 6) {
-      choosen = mystery(temp % 2, " ");
+      choosen = mystery(temp % 2);
     }
     else {
-      choosen = mystery(temp, " ");
+      choosen = mystery(temp);
     }
   }
 
@@ -46,11 +55,12 @@ struct item draw()
 
 
 
-struct item* dailyDraw(struct item list[3]) {
-  struct item three_item[3];
+struct item* dailyDraw() {
+  struct item* three_item;
+  three_item = new struct item [3];
 
   for (int i = 0; i < 3; i++) {
-    list[i] = draw();
+    three_item[i] = draw();
   }
 
   return three_item;
