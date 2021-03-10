be9d1974612312a261f10136f2c920f53392de7a
adhk852 <78186364+adhk852@users.noreply.github.com>
2021-03-10 13:41:43 +0800

    Update player_status.cpp

--- player_status.cpp
+++ player_status.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-// the variable of the player status will be in the main.cpp
+int temp_status[4] = {100, 100, 100, 100};
+// the official array of the player status will be in the main.cpp
 
 // HP, Hydration, Hunger, Mentailty
 void printStatus(int status[4]) {
