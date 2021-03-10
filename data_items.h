ec11080cf3c75efde60e0fcf76c94886ce25cc92
adhk852 <u3574506@connect.hku.hk>
2021-03-10 11:25:28 +0800

    Update data.items.h

--- data_items.h
+++ data_items.h
@@ -6,10 +6,10 @@
 
 #include "structures.h"
 
-struct item water(int x);
-struct item food(int x);
-struct item medicine(int x);
-struct item weapon(int x);
-struct item mystery(int x);
+struct item water(int x, std::string name);
+struct item food(int x, std::string name);
+struct item medicine(int x, std::string name);
+struct item weapon(int x, std::string name);
+struct item mystery(int x, std::string name);
 
 #endif
