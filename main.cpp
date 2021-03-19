471036c1484c50f0ca44ec3b8de52113da795d8a
adhk852 <u3574506@connect.hku.hk>
2021-03-20 01:19:33 +0800

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include "structures.h"
+#include "player_status.h"
+#include "item_pack.h"
+#include "data_items.h"
+#include "draw.h"
+
+using namespace std;
+
+int main() {
+  // starting page: resume game; new game; rules; quit
+
+
+  // resume game;
+
+  // new game;
+
+  // rules;
+
+  // quit
+
+}
