af2f004a7b015592bedbac3b70acd5bfc913276f
yuenkwan <78483446+jojochan09@users.noreply.github.com>
2021-03-21 13:06:30 +0800

    Update item_pack.h

--- item_pack.h
+++ item_pack.h
@@ -1,7 +1,7 @@
 #ifndef ITEM_PACK
 #define ITEM_PACK
 
-void item_menu();
+void item_menu(int water_count[], int food_count[], int medicine_count[], int weapon_count[], int other_count[]);
 void water_menu();
 void food_menu();
 void medicine_menu();
@@ -12,5 +12,6 @@ void use_food(int x);
 void use_medicine(int x);
 void use_weapon(int x);
 void use_other(int x);
+void printEffect(struct item object);
 
 #endif
