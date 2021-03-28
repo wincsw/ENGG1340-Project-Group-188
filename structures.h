f653577eaed40918c1b1ec862a7c65d575834d0f
adhk852 <u3574506@connect.hku.hk>
2021-03-29 00:11:29 +0800

    Update structures.h (changes on attacker.item)

--- structures.h
+++ structures.h
@@ -4,19 +4,30 @@
 #include <iostream>
 
 struct item {
-  std::string name;         // item name
-  int effect[5];            // effect on player status and ATK (weapon only)
-  // [HP, Hydration, Hunger, Mentality, ATK]
-  std::string des;  // item description
+  // item name
+  std::string name;
+  // effect on player status and ATK (weapon only)
+  //  [HP, Hydration, Hunger, Mentality, ATK]         
+  int effect[5];            
+  // item description
+  std::string des;  
 };
 
 struct attacker {
-  std::string name;   // attacker name
-  int hp;             // attacker's hp
-  int atk;            // reduce of player TEMP HP after each attack
-  int lost;           // reduce of player HP if the player lost the fight
-  std::string item;        // item given after winning
-  std::string des;    // attacker description
+  // attacker name
+  std::string name;
+  // attacker's hp   
+  int hp;           
+  // reduce of player TEMP HP after each attack  
+  int atk;            
+  // reduce of player HP if the player lost the fight
+  int lost;           
+  // reward after winning
+  //  [catagory, item_index]
+  //  catagory: 0 = no reward; 1 = food; 2 = weapon
+  int item[2];        
+  // attacker description
+  std::string des;    
 };
 
 struct event {
